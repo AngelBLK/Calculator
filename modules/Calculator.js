c4c5e5bdad47baf79c3dfe6fb4bdde3c8ca26e14
KetzaliF <ketzalihoffman@gmail.com>
2021-10-30 14:36:02 -0500

    update

--- modules/Calculator.js
+++ modules/Calculator.js
@@ -1,37 +1,21 @@
-// class Calculator {
-//   add(num1, num2) {
-//     return num1 + num2;
-//   }
-
-//   sub(num1, num2) {
-//     return num1 - num2;
-//   }
-
-//   mult(num1, num2) {
-//     return num1 * num2;
-//   }
-
-//   divide(num1, num2) {
-//     return num1 / num2;
-//   }
-// }
-
-// export default Calculator;
-
 class Calculadora {
   sumar(num1, num2) {
-      return num1 + num2;
+    return num1 + num2;
   }
 
   restar(num1, num2) {
-      return num1 - num2;
+    return num1 - num2;
   }
 
   dividir(num1, num2) {
-      return num1 / num2;
+    if(num2 === 0){
+        alert('No puedes dividir entre 0, cambia el número!');
+    }else{
+        return num1 / num2;
+    }
   }
 
   multiplicar(num1, num2) {
-      return num1 * num2;
+    return num1 * num2;
   }
 } 
