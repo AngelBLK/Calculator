904c4d2b91365f8b10b1583efc3db13927d7ebef
KetzaliF <ketzalihoffman@gmail.com>
2021-10-31 12:49:22 -0600

    Light theme

---
+++ modules/theme.js
@@ -0,0 +1,13 @@
+const themeBtn = document.getElementById("btn-theme");
+const bgCalculator = document.getElementById("page");
+const bgDisplay = document.querySelector(".display");
+const bgBtn = document.querySelectorAll(".btn");
+
+themeBtn.addEventListener('click', () => {
+   bgCalculator.classList.toggle('light');
+   bgDisplay.classList.toggle('light');
+   bgBtn.forEach(bgBtn => {
+      bgBtn.classList.toggle('light');
+   });
+   themeBtn.classList.toggle('active');
+})
